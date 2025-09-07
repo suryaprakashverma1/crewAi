f552c77e5eeb052cb59c020f82a2875b8c4b0575
suryaprakashverma1 <47498376+suryaprakashverma1@users.noreply.github.com>
2025-09-07 18:59:50 +0530

    Create multiAgentLocalOllamaDockerCrewAI.py

---
+++ multiAgentLocalOllamaDockerCrewAI.py
@@ -0,0 +1,138 @@
+from crewai import Agent, Task, Crew
+from crewai.llm import LLM
+
+# Tell CrewAI to use Ollama as backend
+ollama_llm = LLM(
+    model="ollama/llama3.1:latest",   # specify provider + model
+    base_url="http://host.docker.internal:11434" # point to your running Ollama server
+    #base_url="http://localhost:11434"
+)
+
+##researcher = Agent(
+##    role="Researcher",
+##    goal="Find interesting facts",
+##    backstory="Expert researcher who loves astronomy",
+##    llm=ollama_llm
+##)
+
+planner = Agent(
+    role="Content Planner",
+    goal="Plan engaging and factually accurate content on {topic}",
+    backstory="You're working on planning a blog article "
+              "about the topic: {topic}."
+              "You collect information that helps the "
+              "audience learn something "
+              "and make informed decisions. "
+              "Your work is the basis for "
+              "the Content Writer to write an article on this topic.",
+    llm=ollama_llm,
+    allow_delegation=False,
+	verbose=True
+)
+
+writer = Agent(
+    role="Content Writer",
+    goal="Write insightful and factually accurate "
+         "opinion piece about the topic: {topic}",
+    backstory="You're working on a writing "
+              "a new opinion piece about the topic: {topic}. "
+              "You base your writing on the work of "
+              "the Content Planner, who provides an outline "
+              "and relevant context about the topic. "
+              "You follow the main objectives and "
+              "direction of the outline, "
+              "as provide by the Content Planner. "
+              "You also provide objective and impartial insights "
+              "and back them up with information "
+              "provide by the Content Planner. "
+              "You acknowledge in your opinion piece "
+              "when your statements are opinions "
+              "as opposed to objective statements.",
+    llm=ollama_llm,
+    allow_delegation=False,
+    verbose=True
+)
+
+editor = Agent(
+    role="Editor",
+    goal="Edit a given blog post to align with "
+         "the writing style of the organization. ",
+    backstory="You are an editor who receives a blog post "
+              "from the Content Writer. "
+              "Your goal is to review the blog post "
+              "to ensure that it follows journalistic best practices,"
+              "provides balanced viewpoints "
+              "when providing opinions or assertions, "
+              "and also avoids major controversial topics "
+              "or opinions when possible.",
+     llm=ollama_llm,
+    allow_delegation=False,
+    verbose=True
+)
+
+plan = Task(
+    description=(
+        "1. Prioritize the latest trends, key players, "
+            "and noteworthy news on {topic}.\n"
+        "2. Identify the target audience, considering "
+            "their interests and pain points.\n"
+        "3. Develop a detailed content outline including "
+            "an introduction, key points, and a call to action.\n"
+        "4. Include SEO keywords and relevant data or sources."
+    ),
+    expected_output="A comprehensive content plan document "
+        "with an outline, audience analysis, "
+        "SEO keywords, and resources.",
+    agent=planner,
+)
+
+write = Task(
+    description=(
+        "1. Use the content plan to craft a compelling "
+            "blog post on {topic}.\n"
+        "2. Incorporate SEO keywords naturally.\n"
+		"3. Sections/Subtitles are properly named "
+            "in an engaging manner.\n"
+        "4. Ensure the post is structured with an "
+            "engaging introduction, insightful body, "
+            "and a summarizing conclusion.\n"
+        "5. Proofread for grammatical errors and "
+            "alignment with the brand's voice.\n"
+    ),
+    expected_output="A well-written blog post "
+        "in markdown format, ready for publication, "
+        "each section should have 2 or 3 paragraphs.",
+    agent=writer,
+)
+
+edit = Task(
+    description=("Proofread the given blog post for "
+                 "grammatical errors and "
+                 "alignment with the brand's voice."),
+    expected_output="A well-written blog post in markdown format, "
+                    "ready for publication, "
+                    "each section should have 2 or 3 paragraphs.",
+    agent=editor
+)
+
+crew = Crew(
+    agents=[planner, writer, editor],
+    tasks=[plan, write, edit],
+    #verbose=true
+)
+
+result = crew.kickoff(inputs={"topic": "Artificial Intelligence"})
+
+from IPython.display import Markdown
+Markdown(result.raw)
+
+##task = Task(
+##    description="Collect 5 fun facts about Mars and summarize them.",
+##    expected_output="A short summary containing exactly 5 fun facts about Mars.",
+##    agent=researcher,
+##)
+
+#crew = Crew(agents=[researcher], tasks=[task])
+#result = crew.kickoff()
+#
+#print("Crew Output:\n", result)
